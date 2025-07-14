c0949a99319c47db073758dfa83a541c9eca72c4
sanskriti <sanskritivariya25@gmail.com>
2025-07-14 09:50:05 +0530

    design learn

--- src/components/learnPage/index.tsx
+++ src/components/learnPage/index.tsx
@@ -1,8 +1,52 @@
-import React from 'react';
-import MainLayout from '../../common/mainLayout';
-import { Box } from '@mui/system';
+'use client';
+import React, { useEffect } from 'react';
+
 const LearnMainPage = () => {
-  return <h1>hi</h1>;
+  useEffect(() => {
+    const handleMessage = (event: { data: { type: string; path: any; }; }) => {
+      console.log("📨 Message received from iframe:", event.data);
+
+      if (event.data?.type === "iframeDialogOpen") {
+        console.log("✅ iframeDialogOpen received. Path:", event.data.path);
+      }
+    };
+
+    window.addEventListener("message", handleMessage);
+
+    return () => {
+      window.removeEventListener("message", handleMessage);
+    };
+  }, []);
+
+  return (
+    <>
+      <div className="wrapper">
+        <div className="side-bar">
+          <p>Learn</p>
+          <ul>
+            <li>Introduction</li>
+            <li>Getting Started</li>
+            <li>Advanced Topics</li>
+          </ul>
+        </div>
+
+        <div className="right-section">
+          <div className="header">Header Title</div>
+
+          <div className="content">
+            <h1>Parent Window</h1>
+            <iframe
+              id="react-frame"
+              src="http://localhost:3000/rms"
+              width="800"
+              height="600"
+              title="React Iframe"
+            ></iframe>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default LearnMainPage;
